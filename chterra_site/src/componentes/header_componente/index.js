0537efab898d536140dfe3e8360b18cffe3ff50c
Micheletti <73291937+joaoMicheletti@users.noreply.github.com>
2025-05-07 00:52:05 -0300

    updaate
    update

--- chterra_site/src/componentes/header_componente/index.js
+++ chterra_site/src/componentes/header_componente/index.js
@@ -1,9 +1,36 @@
 import React from "react";
-
+import './style.css';
+import HeaderLogo from './assets/headerLogochterra.png';
+import LogoWhats from './assets/whats.webp';
+import LogoInsta from './assets/instaChterra.webp';
+import LogoLink from './assets/linkdin.webp';
+ 
 export default function HeaderComponent(){
     return(
         <>
-            <h1>Header component</h1>
+            <div id="faixaTopo">
+                <img src={HeaderLogo} alt="logo - nome da empresa" />
+                <nav id="vnavigation">
+                    <a>Dúvidas frequentes | </a>
+                    
+                    <a>Contato | </a>
+                    <a>Trabalhe conosco</a>
+                </nav>
+                <div id="containerIconesRedes">
+                    <img src={LogoWhats} alt="logo da rede social"/>
+                    <img src={LogoInsta} alt="logo da rede social"/>
+                    <img src={LogoLink} alt="logo da rede social"/>
+                </div>
+            </div>
+            <div id="faixaPreta">
+                <nav>
+                    <div className="divMEnu">Área do cliente</div>
+                    <div className="divMEnu">A CHTERRA</div>
+                    <div className="divMEnu">Serviços</div>
+                    <div className="divMEnu">Diferenciais</div>
+                </nav>
+
+            </div>
         </>
     );
 };
